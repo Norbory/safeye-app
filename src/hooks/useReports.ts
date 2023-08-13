82053ce76b5c4fe9f865c9365b56b473eb870bf1
Luis Lopez <lclopez220@gmail.com>
2023-08-13 17:53:27 -0500

    Actualiza useReport
    Obtiene el id de la empresa a la que pertenece el usuario para realizar la llamada de los reportes de la base de datos

--- src/hooks/useReports.ts
+++ src/hooks/useReports.ts
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Report } from "../types";
 import axios from "axios";
+import { useAuth } from "../hooks/useAuth";
 
 import { BASE_URL } from "../config";
 
 export default function useReports() {
   const [reports, setReports] = useState<Report[]>([]);
   const serverUrl = BASE_URL;
+  const { company } = useAuth();
 
   const fetchReports = async () => {
-    const response = await axios.get(
-      `${serverUrl}/api/reports/641631458381e6dbffdc3c51`
-      // TODO: Replace the hardcoded company ID with the one from the context
-    );
+    const response = await axios.get(`${serverUrl}/api/reports/${company}`);
     const reports = response.data.map((report: Report) => ({
       ...report,
       time: new Date(report.time),
