fd920d3434f510e9456a13f207532e84675e7772
Torer0 <enzo.rivas@pucp.edu.pe>
2023-09-24 17:58:28 -0500

    Realice los ajustes finales para el funcionamiento de los botones

---
+++ src/components/Text_Box.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Text, StyleSheet } from 'react-native';
+
+const MarginedTextInput = ({ margin, characterLimit }) => {
+  const [text, setText] = useState('');
+  const [linesWithMargins, setLinesWithMargins] = useState([]);
+
+  useEffect(() => {
+    // Dividir el texto en líneas con el margen dado
+    const lines = text.match(new RegExp(`.{1,${margin}}`, 'g')) || [];
+    setLinesWithMargins(lines);
+  }, [text, margin]);
+
+  const handleTextChange = (newText) => {
+    if (newText.length <= characterLimit) {
+      setText(newText);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textMargin}>Correción</Text>
+      <TextInput
+        style={styles.textInput}
+        multiline={true}
+        placeholder="Escribe aquí..."
+        onChangeText={handleTextChange}
+        value={text}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+  },
+  textMargin: {
+    marginRight: 8, // Espacio entre el texto y el margen derecho
+    marginBottom: 8, // Espacio entre las líneas de texto
+  },
+  textInput: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+  },
+});
+
+export default MarginedTextInput;
