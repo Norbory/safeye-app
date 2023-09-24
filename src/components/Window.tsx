1b1c446f55a47721c521e75d3425e40816f4af6f
Torer0 <enzo.rivas@pucp.edu.pe>
2023-09-24 14:43:12 -0500

    He realizad la logica para los interruptores del modal principal que si se envia los datos, se activa el enviar

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
-import { Modal, StyleSheet, Text, Pressable, View, Switch } from 'react-native';
+import { Modal, StyleSheet, Text, Pressable, View, Switch, Button, TextInput } from 'react-native';
+
+
 
 const CustomModal = ({ isModalVisible, onClose }) => {
+  const [envioModal1, setEnvioModal1] = useState(false);
+  const [envioModal2, setEnvioModal2] = useState(false);
+
+
+
+
   const [switchValue1, setSwitchValue1] = useState(false);
   const [switchValue2, setSwitchValue2] = useState(false);
 
@@ -20,6 +28,7 @@ const CustomModal = ({ isModalVisible, onClose }) => {
     setSwitchValue2(value);
     setModal2Visible(value); // Abrir o cerrar el modal 2
   };
+
 
   // Función para cerrar todos los modales adicionales y los interruptores
   const closeAllModals = () => {
@@ -53,11 +62,12 @@ const CustomModal = ({ isModalVisible, onClose }) => {
 
           {/* Mostrar contenido 1 si modal1Visible es verdadero */}
           {modal1Visible ? (
-            <ModalContent
+            <ModalContent_1
               onClose={() => {
                 setModal1Visible(false);
                 setSwitchValue1(false); // Cerrar el modal 1 y el interruptor 1
               }}
+              setEnvioModal1={setEnvioModal1}
               modalNumber={1}
             />
           ) : null}
@@ -73,15 +83,25 @@ const CustomModal = ({ isModalVisible, onClose }) => {
 
           {/* Mostrar contenido 2 si modal2Visible es verdadero */}
           {modal2Visible ? (
-            <ModalContent
+            <ModalContent_2
               onClose={() => {
                 setModal2Visible(false);
                 setSwitchValue2(false); // Cerrar el modal 2 y el interruptor 2
               }}
+              setEnvioModal2={setEnvioModal2}
               modalNumber={2}
             />
           ) : null}
-
+          
+          <Button
+            title="Enviar"
+            onPress={() => {
+              // Aquí puedes realizar alguna acción con los valores de los interruptores y el cuadro de texto
+              // Cerrar este modal individual
+              onClose();
+            }}
+            disabled = {!envioModal1 || !envioModal2}
+          />
           <Pressable
             style={[styles.button, styles.buttonClose]}
             onPress={() => {
@@ -97,7 +117,12 @@ const CustomModal = ({ isModalVisible, onClose }) => {
 };
 
 // Componente para el contenido del modal individual
-const ModalContent = ({ onClose, modalNumber }) => {
+const ModalContent_1 = ({ onClose, modalNumber, isVisible, setEnvioModal1 }) => {
+  const [switchValue1, setSwitchValue1] = useState(false);
+  const [switchValue2, setSwitchValue2] = useState(false);
+  const [switchValue3, setSwitchValue3] = useState(false);
+  const [textInputValue, setTextInputValue] = useState('');
+
   // Estilos específicos para cada modal
   const modalStyles =
     modalNumber === 1 ? styles.modal1 : modalNumber === 2 ? styles.modal2 : {};
@@ -106,7 +131,7 @@ const ModalContent = ({ onClose, modalNumber }) => {
     <Modal
       animationType="slide"
       transparent={true}
-      visible={true} // Mostrar este modal individual
+      visible={isVisible} // Utiliza la prop isVisible para controlar la visibilidad del modal
       onRequestClose={() => {
         onClose();
       }}>
@@ -114,7 +139,42 @@ const ModalContent = ({ onClose, modalNumber }) => {
         <View style={styles.modalView}>
           <Text style={styles.modalText}>Contenido del Modal {modalNumber}</Text>
 
-          {/* Puedes agregar contenido personalizado aquí */}
+          {/* Agregar 3 interruptores */}
+          <View style={styles.switchContainer}>
+            <Text style={styles.switchLabel}>Interruptor 1</Text>
+            <Switch value={switchValue1} onValueChange={setSwitchValue1} />
+          </View>
+          <View style={styles.switchContainer}>
+            <Text style={styles.switchLabel}>GAAAA 2</Text>
+            <Switch value={switchValue2} onValueChange={setSwitchValue2} />
+          </View>
+          <View style={styles.switchContainer}>
+            <Text style={styles.switchLabel}>Interruptor 3</Text>
+            <Switch value={switchValue3} onValueChange={setSwitchValue3} />
+          </View>
+
+          {/* Agregar cuadro de texto */}
+          <TextInput
+            style={styles.textInput}
+            placeholder="Escribe algo..."
+            onChangeText={(text) => setTextInputValue(text)}
+            value={textInputValue}
+          />
+
+          {/* Agregar botón de enviar */}
+          <Button
+            title="Enviar"
+            onPress={() => {
+              // Aquí puedes realizar alguna acción con los valores de los interruptores y el cuadro de texto
+              console.log('Interruptor 1:', switchValue1);
+              console.log('Interruptor 2:', switchValue2);
+              console.log('Interruptor 3:', switchValue3);
+              console.log('Texto ingresado:', textInputValue);
+              setEnvioModal1(true);
+              // Cerrar este modal individual
+              onClose();
+            }}
+          />
 
           <Pressable
             style={[styles.button, styles.buttonClose]}
@@ -128,7 +188,78 @@ const ModalContent = ({ onClose, modalNumber }) => {
     </Modal>
   );
 };
+// Componente para el contenido del modal individual
+const ModalContent_2 = ({ onClose, modalNumber, isVisible,setEnvioModal2 }) => {
+  const [switchValue1, setSwitchValue1] = useState(false);
+  const [switchValue2, setSwitchValue2] = useState(false);
+  const [switchValue3, setSwitchValue3] = useState(false);
+  const [textInputValue, setTextInputValue] = useState('');
 
+  // Estilos específicos para cada modal
+  const modalStyles =
+    modalNumber === 1 ? styles.modal1 : modalNumber === 2 ? styles.modal2 : {};
+
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isVisible} // Utiliza la prop isVisible para controlar la visibilidad del modal
+      onRequestClose={() => {
+        onClose();
+      }}>
+      <View style={[styles.centeredView, modalStyles]}>
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>Contenido del Modal {modalNumber}</Text>
+
+          {/* Agregar 3 interruptores */}
+          <View style={styles.switchContainer}>
+            <Text style={styles.switchLabel}>Interruptor 1</Text>
+            <Switch value={switchValue1} onValueChange={setSwitchValue1} />
+          </View>
+          <View style={styles.switchContainer}>
+            <Text style={styles.switchLabel}>Interruptor 2</Text>
+            <Switch value={switchValue2} onValueChange={setSwitchValue2} />
+          </View>
+          <View style={styles.switchContainer}>
+            <Text style={styles.switchLabel}>Interruptor 3</Text>
+            <Switch value={switchValue3} onValueChange={setSwitchValue3} />
+          </View>
+
+          {/* Agregar cuadro de texto */}
+          <TextInput
+            style={styles.textInput}
+            placeholder="Escribe algo..."
+            onChangeText={(text) => setTextInputValue(text)}
+            value={textInputValue}
+          />
+
+          {/* Agregar botón de enviar */}
+          <Button
+            title="Enviar"
+            onPress={() => {
+              // Aquí puedes realizar alguna acción con los valores de los interruptores y el cuadro de texto
+              console.log('Interruptor 1:', switchValue1);
+              console.log('Interruptor 2:', switchValue2);
+              console.log('Interruptor 3:', switchValue3);
+              console.log('Texto ingresado:', textInputValue);
+              setEnvioModal2(true);
+              // Cerrar este modal individual
+              onClose();
+            }}
+          />
+
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => {
+              onClose(); // Cerrar este modal individual
+            }}>
+            <Text style={styles.textStyle}>Cerrar Modal</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
+  );
+};
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
@@ -190,6 +321,14 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     marginTop: 10,
+  },
+  textInput: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 5,
+    marginBottom: 10, // Espacio entre el cuadro de texto y los interruptores
   },
 });
 
